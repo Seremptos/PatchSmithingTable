a27828e11e0ba54dc148f802428f9614bf060a2f
Seremptos <thomasf60120@gmail.com>
2023-07-17 08:05:33 +0200

    Patching drag exploit + remove ghost item when removing netherite ingot

--- src/main/java/fr/seremptos/patchsmithingtable/Listener.java
+++ src/main/java/fr/seremptos/patchsmithingtable/Listener.java
@@ -2,6 +2,7 @@ package fr.seremptos.patchsmithingtable;
 
 import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.ViaAPI;
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -9,15 +10,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class Listener implements org.bukkit.event.Listener {
 
+    List<Material> authorizedDiamond = Arrays.asList(
+            Material.DIAMOND_SWORD,
+            Material.DIAMOND_AXE,
+            Material.DIAMOND_HOE,
+            Material.DIAMOND_PICKAXE,
+            Material.DIAMOND_SHOVEL,
+            Material.DIAMOND_BOOTS,
+            Material.DIAMOND_LEGGINGS,
+            Material.DIAMOND_CHESTPLATE,
+            Material.DIAMOND_HELMET);
 
     @EventHandler
     public void onClick(PlayerInteractEvent event){
@@ -29,50 +41,67 @@ public class Listener implements org.bukkit.event.Listener {
         //
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.SMITHING_TABLE){
             LegacySmithingTable smithingTable = new LegacySmithingTable();
-            Bukkit.getScheduler().runTaskLater(PatchSmithingTable.plugin, () -> player.openInventory(smithingTable.getInventory()), 1l);
+            //Delayer l'ouverture du GUI (sinon ne fonctionne pas)
+            Bukkit.getScheduler().runTaskLater(PatchSmithingTable.plugin, () -> player.openInventory(smithingTable.getInventory()), 1L);
         }
     }
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event){
+        if(event.isShiftClick()) event.setCancelled(true);
+        Player player = (Player) event.getWhoClicked();
         Inventory inventory = event.getClickedInventory();
         if(inventory == null) return;
-
-        if(inventory.getItem(0) == null || inventory.getItem(1) == null){
-            inventory.setItem(2, null);
-        }
-
         if(inventory.getHolder() instanceof LegacySmithingTable smithingTable){
-
-            // N'autoriser que la netherite dans le second slot
-            if(event.getSlot() == 1 && event.getCursor().getType() != Material.NETHERITE_INGOT){
-                // Autoriser à reprendre l'item (curseur vide)
-                if(event.getCursor().getType() != Material.AIR){
-                    event.setCancelled(true);
-                }
+            if(inventory.getItem(0) == null || inventory.getItem(1) == null){
+                inventory.setItem(2, null);
             }
-
             switch(event.getSlot()){
                 case 0:
-                    if(inventory.getItem(1) != null) {
+                    // N'autoriser que les outils/armure en diamant dans le premier slot
+                    Material material = event.getCursor().getType();
+
+                    if(!authorizedDiamond.contains(material) && material != Material.AIR){
+                        event.setCancelled(true);
+                    }
+                    if (inventory.getItem(1) != null) {
                         ItemStack item2 = event.getCursor().clone();
                         item2.setType(Material.getMaterial(item2.getType().toString().replace("DIAMOND", "NETHERITE")));
                         inventory.setItem(2, item2);
                     }
-                case 1:
-                    if(inventory.getItem(0) != null){
-                        ItemStack item = inventory.getItem(0).clone();
-                        item.setType(Material.getMaterial(item.getType().toString().replace("DIAMOND", "NETHERITE")));
-                        inventory.setItem(2, item);
-                    }
                     break;
-                case 2:
-                    if(event.getCurrentItem() != null){
-                        inventory.setItem(0, null);
-                        inventory.setItem(1, null);
+            case 1:
+                // N'autoriser que la netherite dans le second slot
+                if(event.getCursor().getType() != Material.NETHERITE_INGOT){
+                    if(event.getCursor().getType() != Material.AIR){
+                        event.setCancelled(true);
+                    }else{
+                        Bukkit.getScheduler().runTaskLater(PatchSmithingTable.plugin, () -> inventory.setItem(2, null), 1L);
                     }
-                    return;
-            }
+                }
+                if(inventory.getItem(0) != null){
+                    ItemStack item = inventory.getItem(0).clone();
+                    item.setType(Material.getMaterial(item.getType().toString().replace("DIAMOND", "NETHERITE")));
+                    inventory.setItem(2, item);
+                }
+                break;
+            case 2:
+                if(event.getCurrentItem() != null){
+                    inventory.setItem(0, inventory.getItem(0).add(-1));
+                    inventory.setItem(1, inventory.getItem(1).add(-1));
+                    player.playSound(Sound.sound(org.bukkit.Sound.BLOCK_SMITHING_TABLE_USE, Sound.Source.BLOCK, 1, 0.9f));
+                }else{
+                    event.setCancelled(true);
+                }
+                break;
+        }
+    }
+}
+
+    @EventHandler
+    public void onDrag(InventoryDragEvent event) {
+        if (event.getInventory().getHolder() instanceof LegacySmithingTable smithingTable) {
+            event.setCancelled(true);
         }
     }
 
